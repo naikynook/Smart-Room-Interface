21f9a85332debcf62c44988b8323ac2501ce95c6
naikynook <codynaiker@gmail.com>
2026-08-01 20:52:49 -0400

    functionality

--- modal_app/deepface_api.py
+++ modal_app/deepface_api.py
@@ -39,7 +39,7 @@ MIN_BOX_FRACTION = 0.002
 ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://codynaiker.github.io",
+    "https://naikynook.github.io",
 ]
 
 # Per-visitor rate limit for /analyze
